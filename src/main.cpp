324e33999dc662d68a93453d195839e271bf5c3b
Ans-A <kafxaaa@gmail.com>
2025-05-25 07:09:54 +0300

    added scanning with output

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 int main(int argc, char* argv[]) {
+    
     if (argc < 2){
         std::cerr << "Usage: " << argv[0] << " <directory_path>\n";
         return 1;
@@ -19,6 +20,22 @@ int main(int argc, char* argv[]) {
 
     }
     std::cout<<"Successfully validated directory"<<target_path<<'\n';
+
+    std::cout << "we're scanning "<<target_path<<'\n';
+
+    try{
+
+        for (const auto& entry: fs::recursive_directory_iterator(target_path)){
+            std::cout << "found: " <<entry.path() << '\n';
+        }
+    }
+
+    catch(const fs::filesystem_error& e){
+        std::cerr << "filesystem inaccessible, you messed up in this: " <<target_path << ":" << e.what() <<'\n';
+        return 1;
+    }
+
+    std::cout<<"Successfully scanned directory"<<target_path<<'\n';
 
     return 0;
 }
