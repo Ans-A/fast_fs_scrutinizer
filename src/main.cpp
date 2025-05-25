9738ab1f86b8a3f92b24e4fb244a6c894f264d85
Ans-A <kafxaaa@gmail.com>
2025-05-25 17:44:56 +0300

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,35 @@
 #include <filesystem>
 #include <optional>
 namespace fs = std::filesystem;
+
+
+bool parseArgs(int argc, char* argv[], fs::path& target_path);
+
+bool validatePath(const fs::path& target_path);
+
+bool scanPathDir(const fs::path& target_path);
+
+
+
 int main(int argc, char* argv[]) {
     
-    if (argc < 2){
-        std::cerr << "Usage: " << argv[0] << " <directory_path>\n";
+    fs::path target_path;
+
+    if(!parseArgs(argc, argv, target_path)){
         return 1;
     }
-    fs::path target_path = argv[1];
+    
+    if(!validatePath(target_path)){
+        return 1;
+    }
+    
+    std::cout << "Path is valid dude, this one ------>" << target_path << '\n';
+
+    if(!scanPathDir(target_path)){
+        return 1;
+    }
+    std::cout << "Scan complete dude, this one ------>" << target_path << '\n';
+
 
     if(!fs::exists(target_path)){
         std::cerr<<"Error: Directory'"<<target_path<<"'does not exist\n";
@@ -43,7 +65,7 @@ int main(int argc, char* argv[]) {
 
 
 
-std::optional<std::string> handleArguments(int argc, char* argv[]){
+std::optional<std::string> parseArgs(int argc, char* argv[]){
 
     if (argc<2){
 
@@ -52,3 +74,33 @@ std::optional<std::string> handleArguments(int argc, char* argv[]){
     }
     return std::string(argv[1]);
 }
+
+bool validate_directory(const fs::path& target_path){
+
+    if(!fs::exists(target_path)){
+        std::cerr << "Error: '" << target_path << "' ain't there, dude\n";
+        return false;
+    }
+        if(!fs::is_directory(target_path)){
+        std::cerr << " Error: '" <<target_path <<"' ain't a dir, dude\n";
+        return false;
+    }
+    return true;
+}
+
+bool scan_dir(const fs::path& target_path) {
+    try{
+        for (const auto& entry : fs::recursive_directory_iterator(target_path)){
+            std::cout << "We got this entry:" << entry.path()<<'\n';
+        }
+    }
+    catch (const fs::filesystem_error& e){
+        std::cerr <<"we have a filesystem error, dude, about: '" << target_path<<"': " << e.what() <<'\n';
+        return false;}
+        return true;
+    }
+
+    
+
+
+
